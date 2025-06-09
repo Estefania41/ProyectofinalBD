0bf3a538af8f0e86f67e9a3645263912b28a6f66
Edisonfr <116932004+Edisonfr@users.noreply.github.com>
2025-06-08 21:24:35 -0500

    Update init.sql

--- football-analytics/init.sql
+++ football-analytics/init.sql
@@ -13,3 +13,13 @@ CREATE TABLE IF NOT EXISTS dim_teams (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+-- Tabla de fechas
+CREATE TABLE IF NOT EXISTS dim_dates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    year INT,
+    month INT,
+    day INT,
+    day_of_week INT,
+    is_weekend BOOLEAN
+);
