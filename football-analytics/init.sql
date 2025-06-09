cb946d1d9d19c62e86cd8e2f49a0b44fb9481aff
Edisonfr <116932004+Edisonfr@users.noreply.github.com>
2025-06-08 21:24:22 -0500

    Update init.sql

--- football-analytics/init.sql
+++ football-analytics/init.sql
@@ -2,3 +2,7 @@
 CREATE DATABASE IF NOT EXISTS futbol_db;
 
 USE futbol_db;
+-- Tabla de competiciones
+CREATE TABLE IF NOT EXISTS dim_competitions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
