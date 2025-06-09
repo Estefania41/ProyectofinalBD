f8804a0f505bfd10fc2051ba3f51a2c199c67418
dnaranjop <117239088+dnaranjop@users.noreply.github.com>
2025-06-09 17:18:37 -0500

    Update import_data.py
    configuracion de la api

--- football-analytics/import_data.py
+++ football-analytics/import_data.py
@@ -4,3 +4,8 @@ import mysql.connector
 from mysql.connector import errorcode
 import requests
 from dotenv import load_dotenv
+load_dotenv()
+
+# Configuración de la API
+API_KEY = '1f21c76952a447f9abf40ace1a8879b4'
+BASE_URL = 'http://api.football-data.org/v4/'
